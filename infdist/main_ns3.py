d35f4dae85821fb76d663fb7238284847ce2ab47
Barcis, Michal <michal.barcis@aau.at>
2020-01-11 14:38:01 +0100

    ns3 now simulates wifi

--- infdist/main_ns3.py
+++ infdist/main_ns3.py
@@ -3,13 +3,31 @@ from simulator import simulator
 from optimization.models import Message
 
 
-def message_received(message):
-    print(message)
-    print(message.data)
+NODES_NUM = 3
+
+counter = {
+    i: 0
+    for i in range(NODES_NUM)
+}
 
 
-net = Network(3)
-net.add_message_received_callback(message_received)
+def gen_message_received(node_id):
+    def message_received(message):
+        global counter
+        print("{} received {}".format(node_id, message))
+        print(message.data)
+        counter[node_id] += 1
+
+    return message_received
+
+
+net = Network(NODES_NUM)
+
+for i in range(NODES_NUM):
+    net.add_message_received_callback(
+        gen_message_received(i),
+        i
+    )
 
 simulator.schedule(0, lambda: net.send(Message(
     0,
@@ -19,21 +37,27 @@ simulator.schedule(0, lambda: net.send(Message(
     data={'name': 'michaŁ'}
 )))
 
-simulator.schedule(1, lambda: net.send(Message(
-    1,
-    {1},
-    0,
-    'batt',
-    data={'name': 'michaŁ'}
-)))
+
+def get_callback(i):
+    def func():
+        print('sending', i)
+        net.send(Message(
+            i % NODES_NUM,
+            {1},
+            0,
+            'batt',
+            data={'name': 'm{}'.format(i)}
+        ))
+    return func
 
 
-simulator.schedule(2, lambda: net.send(Message(
-    2,
-    {1},
-    0,
-    'batt',
-    data={'name': 'michaŁ'}
-)))
+for i in range(100):
+    simulator.schedule(1 + i*.00001, get_callback(i))
 
+
+def print_stats():
+    print(counter)
+
+
+simulator.schedule(3, print_stats)
 simulator.run()
--- infdist/simulator/network.py
+++ infdist/simulator/network.py
@@ -1,10 +1,13 @@
+from collections import defaultdict
 import json
 
 import ns.applications
 import ns.core
 import ns.internet
 import ns.network
+import ns.mobility
 import ns.point_to_point
+import ns.wifi
 
 from . import simulator
 from optimization.models import Message
@@ -31,29 +34,13 @@ class Network:
 
     def __init__(self, nodes_num):
         self.known_data_types = DataTypesStore()
-        self._message_received_callbacks = set()
+        self._message_received_callbacks = defaultdict(set)
 
         self.nodes = ns.network.NodeContainer()
         self.nodes.Create(nodes_num)
 
-        pointToPoint = ns.point_to_point.PointToPointHelper()
-        pointToPoint.SetDeviceAttribute(
-            "DataRate", ns.core.StringValue("5Mbps")
-        )
-        pointToPoint.SetChannelAttribute(
-            "Delay", ns.core.StringValue("2000ms")
-        )
+        self._init_network()
 
-        devices = pointToPoint.Install(self.nodes)
-
-        stack = ns.internet.InternetStackHelper()
-        stack.Install(self.nodes)
-
-        address = ns.internet.Ipv4AddressHelper()
-        address.SetBase(ns.network.Ipv4Address("10.1.1.0"),
-                        ns.network.Ipv4Mask("255.255.255.0"))
-
-        self.interfaces = address.Assign(devices)
         self.sockets = [
             ns.network.Socket.CreateSocket(
                 self.nodes.Get(i),
@@ -68,23 +55,83 @@ class Network:
         broadcast_address = ns.network.InetSocketAddress(
             ns.network.Ipv4Address.GetBroadcast(), self.DATA_PORT
         )
-        for socket in self.sockets:
-            socket.SetRecvCallback(self._ns3_receive_callback)
+        for i, socket in enumerate(self.sockets):
+            socket.SetRecvCallback(self._gen_ns3_receive_callback(i))
             socket.SetAllowBroadcast(True)
             socket.Bind(any_address)
             socket.Connect(broadcast_address)
 
-    def add_message_received_callback(self, callback):
-        self._message_received_callbacks.add(callback)
+    def _init_network(self):
+        rss = -80
+        phy_mode = "DsssRate1Mbps"
 
-    def _message_received(self, message):
-        for callback in self._message_received_callbacks:
+        wifi = ns.wifi.WifiHelper()
+        wifi.SetStandard(ns.wifi.WIFI_PHY_STANDARD_80211b)
+
+        wifi_phy = ns.wifi.YansWifiPhyHelper.Default()
+        wifi_phy.Set("RxGain", ns.core.DoubleValue(0))
+        wifi_phy.SetPcapDataLinkType(
+            ns.wifi.YansWifiPhyHelper.DLT_IEEE802_11_RADIO
+        )
+
+        wifi_channel = ns.wifi.YansWifiChannelHelper()
+        wifi_channel.SetPropagationDelay(
+            "ns3::ConstantSpeedPropagationDelayModel"
+        )
+        # fixed RSS regardless of the distance and transmit power
+        wifi_channel.AddPropagationLoss(
+            "ns3::FixedRssLossModel",
+            "Rss",
+            ns.core.DoubleValue(rss)
+        )
+
+        wifi_phy.SetChannel(wifi_channel.Create())
+
+        # disable rate control
+        wifi.SetRemoteStationManager(
+            "ns3::ConstantRateWifiManager",
+            "DataMode", ns.core.StringValue(phy_mode),
+            "ControlMode", ns.core.StringValue(phy_mode),
+        )
+
+        wifi_mac = ns.wifi.WifiMacHelper()
+        wifi_mac.SetType("ns3::AdhocWifiMac")
+
+        # mobility
+        mobility = ns.mobility.MobilityHelper()
+        positions = ns.mobility.ListPositionAllocator()
+        for i in range(self.nodes.GetN()):
+            positions.Add(ns.mobility.Vector(5*i, int(i/5)*2, 0))
+
+        mobility.SetPositionAllocator(positions)
+        mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel")
+        mobility.Install(self.nodes)
+
+        devices = wifi.Install(wifi_phy, wifi_mac, self.nodes)
+
+        self._init_ip(devices)
+
+    def _init_ip(self, devices):
+        stack = ns.internet.InternetStackHelper()
+        stack.Install(self.nodes)
+
+        address = ns.internet.Ipv4AddressHelper()
+        address.SetBase(ns.network.Ipv4Address("10.1.1.0"),
+                        ns.network.Ipv4Mask("255.255.255.0"))
+
+        address.Assign(devices)
+
+    def add_message_received_callback(self, callback, node_id):
+        self._message_received_callbacks[node_id].add(callback)
+
+    def _message_received(self, message, node_id):
+        for callback in self._message_received_callbacks[node_id]:
             callback(message)
 
-    def _header2message(self, header):
+    def _header2message(self, header, receivers):
         return Message(
             header.GetSender(),
-            receivers=None,
+            receivers=receivers,
             planned_t_sent=None,
             data_type_name=self.known_data_types.get_data_type_name(
                 header.GetDataType()),
@@ -93,14 +140,17 @@ class Network:
             t_rcv=simulator.now()/10**9,
         )
 
-    def _ns3_receive_callback(self, socket):
-        packet = socket.Recv()
-        header = ns.network.InfDistHeader()
-        packet.RemoveHeader(header)
-        # print("received", self._header_repr(header), ns.core.Simulator.Now())
-        self._message_received(
-            self._header2message(header)
-        )
+    def _gen_ns3_receive_callback(self, node_id):
+        def _ns3_receive_callback(socket):
+            packet = socket.Recv()
+            header = ns.network.InfDistHeader()
+            packet.RemoveHeader(header)
+            # print("rcvd", self._header_repr(header), ns.core.Simulator.Now())
+            self._message_received(
+                self._header2message(header, {node_id}),
+                node_id,
+            )
+        return _ns3_receive_callback
 
     @staticmethod
     def _header_repr(header):
